5c24dfa485540bf407255c6530dfaf6d1502478e
Victor Inacio <victorinacio@gmail.com>
2024-02-17 13:33:27 -0300

    Init commit

---
+++ src/app/client.cljs
@@ -0,0 +1,20 @@
+(ns app.client
+  (:require
+    [com.fulcrologic.fulcro.application :as app]
+    [com.fulcrologic.fulcro.components :as comp :refer [defsc]]
+    [com.fulcrologic.fulcro.dom :as dom]
+    [com.fulcrologic.fulcro.algorithms.merge :as merge]
+    [com.fulcrologic.fulcro.algorithms.data-targeting :as targeting]))
+
+(defsc Sample [this {:root/keys [people]}]
+  {}
+  (dom/div "TODO"))
+
+(defonce APP (app/fulcro-app))
+
+(defn ^:export init []
+  (app/mount! APP Sample "app"))
+
+(comment
+
+  )
